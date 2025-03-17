665f2131026d5d9d9d4d3ef6743fb6b9b34639a1
ju4n97 <ju4n97@proton.me>
2025-03-17 05:55:08 -0500

    docs: update README

--- plugins/sqlite/internal/performance_test.go
+++ plugins/sqlite/internal/performance_test.go
@@ -1,5 +1,5 @@
 // SPDX-FileCopyrightText: 2025 The PolyClient Authors
 //
-// SPDX-License-Identifier: GPL-3.0-or-later WITH LicenseRef-PolyClient-Plugin-Exception
+// SPDX-License-Identifier: Apache-2.0
 
 package internal_test
