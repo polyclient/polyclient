0fe6025bbacd21569f266e9d94a1ef41a1a7e3a4
ju4n97 <ju4n97@proton.me>
2025-03-17 05:55:08 -0500

    docs: update README

--- plugins/sqlite/internal/query.go
+++ plugins/sqlite/internal/query.go
@@ -1,6 +1,6 @@
 // SPDX-FileCopyrightText: 2025 The PolyClient Authors
 //
-// SPDX-License-Identifier: GPL-3.0-or-later WITH LicenseRef-PolyClient-Plugin-Exception
+// SPDX-License-Identifier: Apache-2.0
 
 package internal
 
