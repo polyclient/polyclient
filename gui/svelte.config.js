4a880f56cfbe52d75877f4648fe3941cf45bcbde
ju4n97 <ju4n97@proton.me>
2025-03-29 10:09:29 -0500

    feat: add initial SvelteKit setup with routing, localization, and UI components

--- gui/svelte.config.js
+++ gui/svelte.config.js
@@ -1,7 +1,15 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-export default {
-  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
-  // for more information about preprocessors
-  preprocess: vitePreprocess(),
-}
+const config = {
+	preprocess: vitePreprocess(),
+	kit: {
+		adapter: adapter({
+			pages: 'dist',
+			assets: 'dist',
+			fallback: 'index.html',
+		}),
+	},
+};
+
+export default config;
