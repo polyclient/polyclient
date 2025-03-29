7209accdd2b0ea299c9b2d04e7f6a205da8e2eaf
ju4n97 <ju4n97@proton.me>
2025-03-29 10:31:10 -0500

    feat: add UI components including input, label, skeleton, separator, and table elements

---
+++ gui/src/lib/components/ui/form/form-button.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import * as Button from '$lib/components/ui/button/index.js';
+
+	let { ref = $bindable(null), ...restProps }: Button.Props = $props();
+</script>
+
+<Button.Root
+	type="submit"
+	bind:ref
+	{...restProps}
+/>
